e74b9fd6df3f533be1c6344ef2a4e5f32b6e7755
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-11 14:36:46 +0530

    init

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/HospitalManagementSystemBnApplication.java
@@ -0,0 +1,13 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemBnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementSystemBnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/HospitalManagementSystemBnApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemBnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
