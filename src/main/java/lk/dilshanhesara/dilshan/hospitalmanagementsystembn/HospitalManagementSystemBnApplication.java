0ef3fa7d7f8feae8a5289ba64ce57007c576fd5d
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-22 16:47:34 +0530

    spring aoolication

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/HospitalManagementSystemBnApplication.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/HospitalManagementSystemBnApplication.java
@@ -1,5 +1,6 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +14,8 @@ public class HospitalManagementSystemBnApplication {
         SpringApplication.run(HospitalManagementSystemBnApplication.class, args);
     }
 
-
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
