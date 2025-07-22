8f0e48448bd3c2d054b985da3fc84c429b555b20
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-22 16:47:55 +0530

    loagin api con

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/AuthApiController.java
@@ -0,0 +1,42 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.controller;
+
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AuthRequestDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AuthResponseDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthApiController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody AuthRequestDto loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.generateToken(authentication);
+        String role = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).findFirst().orElse(null);
+        return ResponseEntity.ok(new AuthResponseDto(true, "Login Successful!", jwt, role));
+    }
+}
+
