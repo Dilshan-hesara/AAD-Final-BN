1ab4218c1c480593a9faaebe6d0ca46f8a8297e0
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-26 22:09:33 +0530

    jwt token sys up

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/JwtTokenProvider.java
@@ -0,0 +1,49 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.security;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 86400000); // 1 day
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(authToken);
+            return true;
+        } catch (Exception ex) {
+            logger.error("JWT validation error: {}", ex.getMessage());
+        }
+        return false;
+    }
+}
