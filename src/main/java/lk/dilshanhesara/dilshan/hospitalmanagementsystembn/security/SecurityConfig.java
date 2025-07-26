f4e084251d445308fd8ed8157d8cf66322b6a5cf
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-26 22:08:48 +0530

    secu config

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/SecurityConfig.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -23,7 +24,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // Let's go back to using BCrypt for proper security
         return new BCryptPasswordEncoder();
     }
 
@@ -31,7 +31,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -40,6 +39,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/staff/my-profile").authenticated()
+
+                        .requestMatchers("/api/doctors/**").hasAuthority("ROLE_BRANCH_ADMIN")
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
