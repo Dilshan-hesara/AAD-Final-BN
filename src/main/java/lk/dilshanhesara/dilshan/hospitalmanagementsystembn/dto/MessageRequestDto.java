2ecef5e35664615a1d40bf2fb4b8bd70eb7bf186
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-27 21:53:35 +0530

    msg req  dto

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/MessageRequestDto.java
@@ -0,0 +1,9 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
+
+import lombok.Data;
+
+@Data
+public class MessageRequestDto {
+    private Long receiverBranchId;
+    private String content;
+}
