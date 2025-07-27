6b8b3560eaadbbd095f7125c305558366bc10bd5
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-27 21:49:19 +0530

    branch enttiy

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/entity/Branch.java
@@ -0,0 +1,23 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "branches")
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String location;
+
+    @Column(name = "contact_number")
+    private String contactNumber;
+}
