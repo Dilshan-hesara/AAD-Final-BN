57714e662a6a1641bb362f61cba61a005bcdfefb
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-27 21:50:39 +0530

    appoiment enttiy

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/entity/Appointment.java
@@ -0,0 +1,37 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "appointments")
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private Branch branch;
+
+    @Column(name = "appointment_date", nullable = false)
+    private LocalDateTime appointmentDate;
+
+    @Column(nullable = false)
+    private String status;
+
+    private BigDecimal fee;
+}
