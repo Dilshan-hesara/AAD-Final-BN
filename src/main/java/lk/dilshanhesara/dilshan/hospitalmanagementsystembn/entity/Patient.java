8e713b4ddc36f6d99a1d7c5cf4cd52338acf600d
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-27 21:48:22 +0530

    patient enttiy

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/entity/Patient.java
@@ -0,0 +1,35 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "contact_number", nullable = false)
+    private String contactNumber;
+
+    private String email;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    private String gender;
+
+    private String address;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "linked_online_user_id", referencedColumnName = "user_id")
+    private UserAccount linkedOnlineUser; // Optional link to an online account
+}
