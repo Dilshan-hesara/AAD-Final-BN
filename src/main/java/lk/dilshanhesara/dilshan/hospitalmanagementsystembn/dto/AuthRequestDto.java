2b2d8105ad00876671d01ec486c282c1d57e527b
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-27 21:44:12 +0530

    auth req dto

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/AuthRequestDto.java
@@ -0,0 +1,10 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
+
+
+import lombok.Data;
+
+@Data
+public class AuthRequestDto {
+    private String username;
+    private String password;
+}
