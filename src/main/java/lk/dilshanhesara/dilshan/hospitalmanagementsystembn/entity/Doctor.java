34619bc1e3a92a7ff2ccd31d480d12ba147becd0
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-27 21:48:39 +0530

    doc enttiy

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/entity/Doctor.java
@@ -0,0 +1,28 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "doctors")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String fullName;
+    private String email;
+    private String contactNumber;
+    private String specialization;
+
+    // --- ADD THIS LINE ---
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id")
+    private Branch branch;
+}
