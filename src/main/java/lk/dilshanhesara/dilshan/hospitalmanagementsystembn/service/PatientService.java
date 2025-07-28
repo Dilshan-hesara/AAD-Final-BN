7adc635d127223ee33236f0a5107c0e369a1ab82
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-28 09:46:15 +0530

    patien serve

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/PatientService.java
@@ -0,0 +1,8 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.PatientDto;
+
+public interface PatientService {
+    void addPatient(PatientDto patientDto);
+}
+
