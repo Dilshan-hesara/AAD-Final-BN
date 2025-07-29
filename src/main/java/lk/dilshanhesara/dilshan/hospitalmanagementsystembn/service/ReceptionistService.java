97b28f2a5dfb382f63ffeab96e64e50921f89cdc
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-29 13:47:16 +0530

    reception save & get all

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/ReceptionistService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/ReceptionistService.java
@@ -1,4 +1,13 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffCreationRequestDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffProfileDto;
+
+import java.util.List;
+
 public interface ReceptionistService {
+
+
+    List<StaffProfileDto> findReceptionistsByBranch(Long branchId);
+    void addReceptionist(StaffCreationRequestDto receptionistDto);
 }
