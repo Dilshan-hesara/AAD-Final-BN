62e06149c609b135d2f49362afba10de7a7e2407
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-29 16:33:01 +0530

    appoiment resp dto

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/AppointmentResponseDto.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/AppointmentResponseDto.java
@@ -5,10 +5,12 @@ import java.time.LocalDateTime;
 
 @Data
 public class AppointmentResponseDto {
+
+
+
     private Long id;
     private String patientName;
     private String doctorName;
-    private String branchName;
     private LocalDateTime appointmentDate;
     private String status;
 }
