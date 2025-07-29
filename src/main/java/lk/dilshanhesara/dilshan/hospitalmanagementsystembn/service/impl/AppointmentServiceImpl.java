0210041542e30d66a7d63218e906c8186033bc5b
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-29 16:32:28 +0530

    appoiment service impl

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/impl/AppointmentServiceImpl.java
@@ -0,0 +1,6 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.impl;
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.AppointmentService;
+
+public class AppointmentServiceImpl implements AppointmentService {
+}
