07834ffb85e89ecea1b14ebc74add59991448039
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-29 13:47:11 +0530

    reception save & get all

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/ReceptionistApiController.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/ReceptionistApiController.java
@@ -1,4 +1,39 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.controller;
 
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffCreationRequestDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffProfileDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.ReceptionistService;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.StaffProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/receptionists")
+@RequiredArgsConstructor
 public class ReceptionistApiController {
+
+    private final ReceptionistService receptionistService;
+    private final StaffProfileService staffProfileService;
+
+    @GetMapping
+    public ResponseEntity<List<StaffProfileDto>> getReceptionistsForCurrentBranch() {
+        StaffProfileDto adminProfile = staffProfileService.findStaffByUsername(
+                org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication().getName()
+        );
+        List<StaffProfileDto> receptionists = receptionistService.findReceptionistsByBranch(adminProfile.getBranchId());
+        return ResponseEntity.ok(receptionists);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addReceptionist(@RequestBody StaffCreationRequestDto receptionistDto) {
+        StaffProfileDto adminProfile = staffProfileService.findStaffByUsername(
+                org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication().getName()
+        );
+        receptionistDto.setBranchId(adminProfile.getBranchId()); // Auto-assign branch
+        receptionistService.addReceptionist(receptionistDto);
+        return ResponseEntity.ok().build();
+    }
 }
