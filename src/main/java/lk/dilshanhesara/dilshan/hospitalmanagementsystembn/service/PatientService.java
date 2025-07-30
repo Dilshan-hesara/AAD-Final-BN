49bb1e07aa2fe569d2710dc70bb5b374196ceb10
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-30 05:42:28 +0530

    loadd all patient fro appoment

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/PatientService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/PatientService.java
@@ -2,7 +2,13 @@ package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.PatientDto;
 
+import java.util.List;
+
 public interface PatientService {
     void addPatient(PatientDto patientDto);
-}
+
+
+    public List<PatientDto> getAllPatients() ;
+
+    }
 
