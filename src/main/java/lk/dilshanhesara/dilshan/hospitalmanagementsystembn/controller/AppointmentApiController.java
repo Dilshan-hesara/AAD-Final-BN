6a0c9cf11f1e1a783e801d978cd40fdc7cbb01bc
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-30 05:41:39 +0530

    appoiment con get & save

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/AppointmentApiController.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/AppointmentApiController.java
@@ -1,4 +1,36 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.controller;
 
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AppointmentRequestDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AppointmentResponseDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffProfileDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.AppointmentService;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.StaffProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointments")
+@RequiredArgsConstructor
 public class AppointmentApiController {
+
+    private final AppointmentService appointmentService;
+    private final StaffProfileService staffProfileService;
+
+    @GetMapping
+    public ResponseEntity<List<AppointmentResponseDto>> getAppointmentsForBranch() {
+        StaffProfileDto adminProfile = staffProfileService.getCurrentLoggedInStaffProfile();
+        List<AppointmentResponseDto> appointments = appointmentService.findAppointmentsByBranch(adminProfile.getBranchId());
+        return ResponseEntity.ok(appointments);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createAppointment(@RequestBody AppointmentRequestDto dto) {
+        StaffProfileDto adminProfile = staffProfileService.getCurrentLoggedInStaffProfile();
+        appointmentService.createAppointment(dto, adminProfile.getBranchId());
+        return ResponseEntity.ok().build();
+    }
 }
