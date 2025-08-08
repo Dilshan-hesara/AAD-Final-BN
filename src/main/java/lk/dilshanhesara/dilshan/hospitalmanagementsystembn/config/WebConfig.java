658f7968562d6a782b5c4d8b2af192bd7f3a9be6
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-08 21:33:46 +0530

    web config up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/config/WebConfig.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/config/WebConfig.java
@@ -16,4 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
+
+
+
 }
