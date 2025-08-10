a8f2d00f9cf36230e1c53441b4a548600a0bef4f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-11 04:37:38 +0530

    patient loadd appiment book

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/PatientRepository.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/PatientRepository.java
@@ -1,7 +1,10 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.repo;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,6 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Optional<Patient> findByLinkedOnlineUser_UserId(Integer userId);
 
+    @Query("SELECT DISTINCT p FROM Patient p JOIN Appointment a ON p.id = a.patient.id WHERE a.branch.id = :branchId")
+    Page<Patient> findPatientsByBranch(Long branchId, Pageable pageable);
 }
