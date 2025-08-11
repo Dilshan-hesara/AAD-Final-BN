cd740faa46c267590ba142f55f3d1083172d8382
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-11 05:49:34 +0530

    settign up free

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/SettingsService.java
@@ -0,0 +1,11 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
+
+import java.math.BigDecimal;
+
+public interface SettingsService {
+
+
+
+    BigDecimal getAppointmentFee();
+    void updateAppointmentFee(BigDecimal newFee);
+}
---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/impl/SettingsServiceImpl.java
@@ -0,0 +1,36 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.impl;
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.AppSetting;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.repo.AppSettingsRepository;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.SettingsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class SettingsServiceImpl implements SettingsService {
+
+    // In a real app, this value would be loaded from a database table.
+    private final AppSettingsRepository settingsRepository;
+    private final String FEE_KEY = "APPOINTMENT_FEE";
+
+    @Override
+    public BigDecimal getAppointmentFee() {
+        return settingsRepository.findById(FEE_KEY)
+                .map(setting -> new BigDecimal(setting.getSettingValue()))
+                .orElse(new BigDecimal("2500.00")); // Fallback default
+    }
+
+    @Override
+    public void updateAppointmentFee(BigDecimal newFee) {
+        AppSetting feeSetting = settingsRepository.findById(FEE_KEY)
+                .orElse(new AppSetting());
+
+        feeSetting.setSettingKey(FEE_KEY);
+        feeSetting.setSettingValue(newFee.toString());
+        settingsRepository.save(feeSetting);
+    }
+
+}
