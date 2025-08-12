aa2675aafbe35ebba1107490e2f4116554e3dd2c
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-12 21:52:26 +0530

    resp service up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/ReceptionistService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/ReceptionistService.java
@@ -2,6 +2,9 @@ package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffCreationRequestDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffProfileDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.UserAccount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -12,4 +15,13 @@ public interface ReceptionistService {
     void addReceptionist(StaffCreationRequestDto receptionistDto);
 
     public void updateUserStatus(Integer userId, boolean isActive) ;
+
+
+
+    public Page<StaffProfileDto> searchReceptionists(Long branchId, String name, Pageable pageable) ;
+    public void updateReceptionist(Integer userId, StaffProfileDto dto);
+
+    // Helper method to convert UserAccount to StaffProfileDto
+     StaffProfileDto convertToStaffProfileDto(UserAccount account) ;
+
 }
