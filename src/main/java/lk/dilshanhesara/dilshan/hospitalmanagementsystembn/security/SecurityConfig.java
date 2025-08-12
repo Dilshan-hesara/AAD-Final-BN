c79b4a1af3bedba5874741bef5d7e020fda44c0f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-12 21:51:12 +0530

    sec  ci up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/SecurityConfig.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.security;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +24,7 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -31,25 +34,23 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> {})
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Set to stateless
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // Login/Register
-                        .anyRequest().authenticated() // All
-                        // other requests require a valid token
-
-
+                        // Allow everyone to access the login and registration APIs
+                        .requestMatchers("/api/auth/**").permitAll()
+                        // All other requests must be authenticated
+                        .anyRequest().authenticated()
                 )
-
+                // Add the JWT filter to the chain
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-
 
 }
