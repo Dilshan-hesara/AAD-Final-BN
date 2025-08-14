6754736bbc344cd99653f196bdca4b3c50024625
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-14 20:05:13 +0530

    branch  dto up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/BranchDto.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/BranchDto.java
@@ -6,4 +6,8 @@ import lombok.Data;
 public class BranchDto {
     private Long id;
     private String name;
+
+    private String location;
+    private String contactNumber;
+    private String email; // Added email field
 }
