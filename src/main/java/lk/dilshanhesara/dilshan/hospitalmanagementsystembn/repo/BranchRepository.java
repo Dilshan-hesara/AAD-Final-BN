668025f42d5cbb286563b55053a864dd764adf5d
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-15 17:18:45 +0530

    super branch status

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/BranchRepository.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/BranchRepository.java
@@ -4,8 +4,12 @@ import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
+
+    List<Branch> findByStatus(String status);
 
 
 }
