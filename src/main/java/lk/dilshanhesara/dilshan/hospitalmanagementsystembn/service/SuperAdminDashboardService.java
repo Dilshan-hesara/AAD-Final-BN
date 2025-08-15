e392728eea1721f4755904751ba302bd900300b0
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-15 21:22:05 +0530

    Super dash details repo

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/SuperAdminDashboardService.java
@@ -0,0 +1,7 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.SuperAdminDashboardDto;
+
+public interface SuperAdminDashboardService {
+    SuperAdminDashboardDto getDashboardSummary();
+}
