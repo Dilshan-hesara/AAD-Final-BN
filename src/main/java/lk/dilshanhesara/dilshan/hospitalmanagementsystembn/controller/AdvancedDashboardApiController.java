3a4cf8435a27e7da217ca7af292c5d67a31c52ed
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-16 20:57:38 +0530

    super branch advance con api add summey

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/AdvancedDashboardApiController.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/controller/AdvancedDashboardApiController.java
@@ -1,4 +1,26 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.controller;
 
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AdvancedBranchDashboardDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.Appointment;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.AdvancedDashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/branch-dashboard")
+@RequiredArgsConstructor
 public class AdvancedDashboardApiController {
+    private final AdvancedDashboardService advancedDashboardService;
+    @GetMapping("/{branchId}")
+    public ResponseEntity<AdvancedBranchDashboardDto> getAdvancedSummary(
+            @PathVariable Long branchId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
+
+        return ResponseEntity.ok(advancedDashboardService.getAdvancedDashboardSummary(branchId, startDate, endDate));
+    }
 }
