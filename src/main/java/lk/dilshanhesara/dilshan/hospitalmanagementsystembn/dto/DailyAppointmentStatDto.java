bf43c00bc03113b0b696f572c102a5da3349892c
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-16 20:57:03 +0530

    Super dash details dto up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/DailyAppointmentStatDto.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/DailyAppointmentStatDto.java
@@ -1,4 +1,35 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DailyAppointmentStatDto {
+    private String date;
+    private long count;
+
+    public DailyAppointmentStatDto(long count, String date) {
+        this.count = count;
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
 }
+
