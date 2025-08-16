56a05bedbadb84d5bc70d787cf8093716c17d9ec
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-16 20:57:43 +0530

    super branch advance con api add summey

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/AdvancedDashboardService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/AdvancedDashboardService.java
@@ -1,4 +1,11 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AdvancedBranchDashboardDto;
+
+import java.time.LocalDateTime;
+
 public interface AdvancedDashboardService {
-}
+
+    public AdvancedBranchDashboardDto getAdvancedDashboardSummary(Long branchId, LocalDateTime startDate, LocalDateTime endDate) ;
+
+    }
