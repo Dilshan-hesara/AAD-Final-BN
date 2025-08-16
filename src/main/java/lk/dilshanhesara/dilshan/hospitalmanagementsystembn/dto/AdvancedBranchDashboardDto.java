12c4c10034f3dd6190f21b8eba3e269ad4fdaa13
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-16 20:57:11 +0530

    Super dash details dto up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/AdvancedBranchDashboardDto.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/AdvancedBranchDashboardDto.java
@@ -1,4 +1,24 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor // <-- ADD THIS ANNOTATION
+@AllArgsConstructor
 public class AdvancedBranchDashboardDto {
+    private double revenue;
+    private long newPatientCount;
+    private long confirmedAppointments;
+    private long completedAppointments;
+    private long onlineBookings;
+    private long walkInBookings;
+    private List<TopDoctorDto> topDoctors; // New DTO for top doctors
+    private List<DailyAppointmentStatDto> appointmentTrend; // New DTO for line chart
+
+
+
+
 }
