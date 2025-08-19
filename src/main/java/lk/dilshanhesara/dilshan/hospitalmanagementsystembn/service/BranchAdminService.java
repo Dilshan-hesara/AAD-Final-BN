e0ce5be85df615fa0dcfdd6a80d8f11b12cf6c40
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-19 18:24:59 +0530

    suoer admin  add  add branch admin edit add

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/BranchAdminService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/BranchAdminService.java
@@ -1,5 +1,6 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.AdminUpdateRequestDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffCreationRequestDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffProfileDto;
 import org.springframework.data.domain.Page;
@@ -10,6 +11,6 @@ public interface BranchAdminService {
 
     Page<StaffProfileDto> getAllBranchAdmins(Pageable pageable);
     void addBranchAdmin(StaffCreationRequestDto dto);
-    void updateBranchAdmin(Integer userId, StaffProfileDto dto);
+    public void updateBranchAdmin(Integer userId, AdminUpdateRequestDto dto) ;
     void deleteBranchAdmin(Integer userId);
 }
