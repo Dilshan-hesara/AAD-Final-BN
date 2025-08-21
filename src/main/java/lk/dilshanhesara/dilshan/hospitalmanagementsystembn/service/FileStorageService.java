f61d2ebf885b76650ecf2e52fa1f1307ef12862f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-22 01:44:33 +0530

    potos save

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/FileStorageService.java
@@ -0,0 +1,29 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation = Paths.get("uploads").toAbsolutePath().normalize();
+
+    public FileStorageService() {
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not create the directory for uploads.", ex);
+        }
+    }
+
+
+}
