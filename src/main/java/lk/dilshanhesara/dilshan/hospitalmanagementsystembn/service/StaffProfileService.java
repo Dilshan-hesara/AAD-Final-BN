2301731436a59de002840849d44003e2c831aed6
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-22 01:45:06 +0530

    staff uyp

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/StaffProfileService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/StaffProfileService.java
@@ -3,6 +3,7 @@ package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.PasswordChangeDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.StaffProfileDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.UserAccount;
 
 public interface StaffProfileService {
     StaffProfileDto findStaffByUsername(String username);
@@ -18,6 +19,7 @@ public interface StaffProfileService {
 
     public void changePassword(String username, PasswordChangeDto passwordDto) ;
 
+     StaffProfileDto convertToStaffProfileDto(UserAccount account) ;
 
 
 }
