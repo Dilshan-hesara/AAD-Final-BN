d1d1086269afa561d5e38f30e554c8bfa6ff2e13
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-08-30 09:32:35 +0530

    online user dash dto

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/OnlineUserDashboardDto.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/OnlineUserDashboardDto.java
@@ -6,7 +6,7 @@ import java.util.List;
 @Data
 public class OnlineUserDashboardDto {
     private String fullName;
-    private String patientId; // This can be the online_user_id or another identifier
+    private String patientId;
     private String profilePictureUrl;
     private AppointmentResponseDto upcomingAppointment;
     private List<AppointmentResponseDto> recentActivity;
