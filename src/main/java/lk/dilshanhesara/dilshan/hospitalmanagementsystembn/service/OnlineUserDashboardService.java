8e99d664a820e909b3933c54225b597fa95a8d3d
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-09-03 19:05:53 +0530

    onlin user dash creat

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/OnlineUserDashboardService.java
@@ -0,0 +1,8 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
+
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.OnlineUserDashboardDto;
+
+public interface OnlineUserDashboardService {
+    OnlineUserDashboardDto getDashboardData(String username);
+
+}
