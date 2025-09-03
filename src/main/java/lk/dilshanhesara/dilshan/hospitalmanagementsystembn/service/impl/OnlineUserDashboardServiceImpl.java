7497aa8979b599f38ed39f49c1484d7a06826ad0
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-09-03 19:06:29 +0530

    onlin user dashimpl up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/impl/OnlineUserDashboardServiceImpl.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/impl/OnlineUserDashboardServiceImpl.java
@@ -1,5 +1,7 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service.impl;
 
+@Service
+@RequiredArgsConstructor
 public class OnlineUserDashboardServiceImpl implements OnlineUserDashboardService {
 
 
