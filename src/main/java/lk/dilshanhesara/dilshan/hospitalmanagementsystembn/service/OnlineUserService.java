00760b4cdcc46b1e2f65340eb5a0814a01406f7a
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-09-04 23:39:48 +0530

    onlin user upadet profile

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/OnlineUserService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/OnlineUserService.java
@@ -1,10 +1,15 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.OnlineUserRegistrationDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.PasswordChangeDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.UserProfileDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface OnlineUserService {
     UserProfileDto findProfileByUsername(String username);
     void registerNewOnlineUser(OnlineUserRegistrationDto registrationDto);
+
+
+    public void updateMyProfile(String username, UserProfileDto dto);
 
 }
