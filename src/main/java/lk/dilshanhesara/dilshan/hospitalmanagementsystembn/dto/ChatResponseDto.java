e5fe1cf576a5845e353d4bb22158a1d8fb66f938
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-09-06 22:04:42 +0530

    ai caht respon dto

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/ChatResponseDto.java
@@ -0,0 +1,9 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class ChatResponseDto {
+    private String reply;
+}
