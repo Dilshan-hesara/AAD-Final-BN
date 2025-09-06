4957e6e34f910b54c842c6525377e67896cfc062
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-09-06 22:04:22 +0530

    web sec up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/SecurityConfig.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/security/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity // <-- ADD THIS ANNOTATION
+@EnableMethodSecurity
 
 public class SecurityConfig {
 
