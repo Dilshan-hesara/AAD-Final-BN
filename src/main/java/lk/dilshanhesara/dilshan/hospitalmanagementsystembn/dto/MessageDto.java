fff48be34973806e102776fb43923f0ad21847f1
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-08 02:05:53 +0530

    msg dto

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/MessageDto.java
@@ -0,0 +1,12 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+@Data
+public class MessageDto {
+    private Long id;
+    private Long senderBranchId;
+    private String senderBranchName;
+    private String content;
+    private LocalDateTime timestamp;
+}
