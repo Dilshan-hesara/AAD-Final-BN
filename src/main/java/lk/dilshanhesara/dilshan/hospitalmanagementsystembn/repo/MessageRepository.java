2e36e0a85dac7f390edfc9dc19639f079fdf08d4
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-09 18:01:16 +0530

    msg sendiig up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/MessageRepository.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/MessageRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 public interface MessageRepository extends JpaRepository<Message, Long> {
+//    @Query("SELECT m FROM Message m WHERE (m.senderBranch.id = :branch1Id AND m.receiverBranch.id = :branch2Id) OR (m.senderBranch.id = :branch2Id AND m.receiverBranch.id = :branch1Id) ORDER BY m.timestamp ASC")
+//    List<Message> findConversation(Long branch1Id, Long branch2Id);
+
+
     @Query("SELECT m FROM Message m WHERE (m.senderBranch.id = :branch1Id AND m.receiverBranch.id = :branch2Id) OR (m.senderBranch.id = :branch2Id AND m.receiverBranch.id = :branch1Id) ORDER BY m.timestamp ASC")
     List<Message> findConversation(Long branch1Id, Long branch2Id);
+
 }
