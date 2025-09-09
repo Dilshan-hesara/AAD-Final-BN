0774e85a4c867424738269e272fcc11755b8d2bb
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-09 19:22:19 +0530

    msg sendiig servis up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/MessageService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/MessageService.java
@@ -3,6 +3,8 @@ package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.MessageDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.MessageRequestDto;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.UserAccount;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 public interface MessageService {
@@ -14,4 +16,9 @@ public interface MessageService {
 
     List<MessageDto> getConversation(Long myBranchId, Long otherBranchId);
     void sendMessage(Integer senderUserId, MessageRequestDto dto);
+
+
+
+    public long getUnreadMessageCount(Long branchId, UserAccount.Role role) ;
+    public void markConversationAsRead(Long myBranchId, Long otherBranchId, UserAccount.Role myRole) ;
     }
