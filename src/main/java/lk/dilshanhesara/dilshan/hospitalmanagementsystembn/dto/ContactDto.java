2f4e75007bbbfae30d2cd56de4296ed53df810f0
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-09 21:09:14 +0530

    msg sendiig servis up

---
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/ContactDto.java
@@ -0,0 +1,9 @@
+package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
+
+import lombok.Data;
+@Data
+public class ContactDto {
+    private Long branchId;
+    private String branchName;
+    private String role;
+}
