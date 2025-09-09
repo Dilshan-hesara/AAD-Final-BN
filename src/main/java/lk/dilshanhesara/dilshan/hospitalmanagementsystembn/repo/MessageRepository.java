cb5fc63625fa6709d4ea8649fabb02968912cfbf
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-09 19:21:57 +0530

    msg sendiig repo up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/MessageRepository.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/MessageRepository.java
@@ -1,6 +1,7 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.repo;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.Message;
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
@@ -11,5 +12,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Query("SELECT m FROM Message m WHERE (m.senderBranch.id = :branch1Id AND m.receiverBranch.id = :branch2Id) OR (m.senderBranch.id = :branch2Id AND m.receiverBranch.id = :branch1Id) ORDER BY m.timestamp ASC")
     List<Message> findConversation(Long branch1Id, Long branch2Id);
+
+    long countByReceiverBranchIdAndRecipientRoleAndIsReadFalse(Long branchId, UserAccount.Role role);
 
 }
