34ac2e973a8a4d3db698284d99629401bbc550e1
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-10 20:45:48 +0530

    cotact dto

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/ContactDto.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/dto/ContactDto.java
@@ -1,9 +1,18 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactDto {
     private Long branchId;
     private String branchName;
     private String role;
+
+    private Integer userId;
+    private String fullName;
+
 }
