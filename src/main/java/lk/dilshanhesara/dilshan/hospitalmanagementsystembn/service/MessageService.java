d7633eddd654ce6578ad4736518c633855ef5ee1
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-10 20:48:12 +0530

    msg ser up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/MessageService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/MessageService.java
@@ -5,7 +5,6 @@ import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.ContactDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.MessageDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.dto.MessageRequestDto;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.UserAccount;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 public interface MessageService {
@@ -24,11 +23,23 @@ public interface MessageService {
     public void markConversationAsRead(Long myBranchId, Long otherBranchId, UserAccount.Role myRole) ;
 
 
-    List<ContactDto> getContacts(Long myBranchId);
+    List<ContactDto> getContacts(Integer userId, Long myBranchId);
     List<MessageDto> getConversation(Long myBranchId, UserAccount.Role myRole, Long otherBranchId, UserAccount.Role otherRole);
     void sendMessage(Integer senderUserId, MessageRequestDto dto);
 
 
+    public List<ContactDto> getReceptionistContacts(Long branchId, Integer myUserId) ;
+    public List<MessageDto> getConversation(Integer userId1, Integer userId2) ;
 
 
-    }
+
+
+
+    List<ContactDto> getSuperAdminContacts();
+
+
+
+    public void sendMessageSuper(Integer senderUserId, MessageRequestDto dto) ;
+
+
+}
