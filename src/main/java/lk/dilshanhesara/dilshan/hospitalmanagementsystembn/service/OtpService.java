3f24c8d37a84720fc5abecc0af3f039d3ab6fa29
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-13 20:57:55 +0530

    otp serveis

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/OtpService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/OtpService.java
@@ -3,16 +3,19 @@ package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.service;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.OtpCode;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.UserAccount;
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.repo.OtpCodeRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public interface OtpService {
 
 
 
-
-
-
-
-
+    public void generateAndSendOtp(String username) ;
+    public boolean verifyOtp(String username, String code);
+    public void resetPassword(String username, String newPassword) ;
 
 
 }
