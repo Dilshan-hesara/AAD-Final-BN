9c5f1c25389fdb29362df5cd00bd42192acd77cc
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-16 22:41:57 +0530

    notification repo up

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/NotificationRepository.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/NotificationRepository.java
@@ -1,4 +1,14 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.repo;
 
+import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.Notification;
+
+import java.util.List;
+
 public interface NotificationRepository {
+
+
+
+    List<Notification> findByUser_UsernameAndIsReadFalseOrderByCreatedAtDesc(String username);
+
+
 }
