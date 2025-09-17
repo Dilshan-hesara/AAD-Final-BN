ea2e3a4c72984e2e50a2d975983b8cae597e7e74
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-17 19:03:36 +0530

    eamil servis up add send emal

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/EmailService.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/service/EmailService.java
@@ -22,34 +22,5 @@ public class EmailService {
 
 
 
-    public void sendAppointmentReminderEmail(String toEmail, Appointment appointment) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(toEmail);
-            message.setSubject("Upcoming Appointment Reminder - HMS");
 
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("eeee, MMMM d 'at' h:mm a");
-            String formattedDate = appointment.getAppointmentDate().format(formatter);
-
-            String text = String.format(
-                    "Dear %s,\n\n" +
-                            "This is a friendly reminder for your upcoming appointment.\n\n" +
-                            "Doctor: %s\n" +
-                            "Branch: %s\n" +
-                            "Date & Time: %s\n\n" +
-                            "We look forward to seeing you.\n\n" +
-                            "Sincerely,\nThe HMS Team",
-                    appointment.getPatient().getFullName(),
-                    appointment.getDoctor().getFullName(),
-                    appointment.getBranch().getName(),
-                    formattedDate
-            );
-
-            message.setText(text);
-            mailSender.send(message);
-            System.out.println("EMAIL SENT: Reminder sent to " + toEmail);
-        } catch (Exception e) {
-            System.err.println("EMAIL FAILED: Could not send reminder to " + toEmail + ". Error: " + e.getMessage());
-        }
-    }
 }
