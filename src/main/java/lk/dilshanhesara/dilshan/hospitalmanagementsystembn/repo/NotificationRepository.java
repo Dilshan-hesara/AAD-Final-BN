e86ade1ac67777b8604982b438552ba14ab07396
Dilshan-hesara <hesarsdilshan@gamil.com>
2025-09-17 19:03:18 +0530

    notification servis up add send emal

--- src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/NotificationRepository.java
+++ src/main/java/lk/dilshanhesara/dilshan/hospitalmanagementsystembn/repo/NotificationRepository.java
@@ -1,14 +1,16 @@
 package lk.dilshanhesara.dilshan.hospitalmanagementsystembn.repo;
 
 import lk.dilshanhesara.dilshan.hospitalmanagementsystembn.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-public interface NotificationRepository {
 
-
-
+//    List<Notification> findByUser_UsernameAndIsReadFalseOrderByCreatedAtDesc(String username);
+    boolean existsByAppointmentId(Long appointmentId);
     List<Notification> findByUser_UsernameAndIsReadFalseOrderByCreatedAtDesc(String username);
-
 
 }
